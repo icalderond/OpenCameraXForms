fc0340e5ec1b243445d5f103f6ef98ad32c74985
icalderond <icalderond@outlook.com>
2017-08-29 10:42:15 -0500

    Primer commit e instalar  xam.plugin.media

---
+++ testCamera_xforms/testCamera_xformsPage.xaml.cs
@@ -0,0 +1,12 @@
+﻿using Xamarin.Forms;
+
+namespace testCamera_xforms
+{
+    public partial class testCamera_xformsPage : ContentPage
+    {
+        public testCamera_xformsPage()
+        {
+            InitializeComponent();
+        }
+    }
+}

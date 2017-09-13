236e5aff4a8caac6f55b0eca1470f006c09d9f08
icalderond <icalderond@outlook.com>
2017-09-13 11:12:15 -0500

    Abriendo camara

--- testCamera_xforms/testCamera_xformsPage.xaml.cs
+++ testCamera_xforms/testCamera_xformsPage.xaml.cs
@@ -1,4 +1,7 @@
-﻿using Xamarin.Forms;
+﻿using System;
+using Plugin.Media;
+using Plugin.Media.Abstractions;
+using Xamarin.Forms;
 
 namespace testCamera_xforms
 {
@@ -7,6 +10,14 @@ namespace testCamera_xforms
         public testCamera_xformsPage()
         {
             InitializeComponent();
+
+            CameraButton.Clicked += CameraButton_Clicked;
+        }
+
+        private async void CameraButton_Clicked(object sender, EventArgs e)
+        {
+            var photo = await CrossMedia.Current.TakePhotoAsync(new StoreCameraMediaOptions() { });
+            if (photo != null) PhotoImage.Source = ImageSource.FromStream(photo.GetStream);
         }
     }
 }
